b68388df049395c4653fcfa38b31874270927a7d
Vinicius Noli <viniciusnoli2016@gmail.com>
2023-08-01 17:13:43 -0300

    feat: mounted server structure

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,31 @@
 import Fastify from 'fastify';
+import cors from '@fastify/cors';
+import jwt from '@fastify/jwt';
+import multipart from '@fastify/multipart';
+import { resolve } from 'node:path';
 
-const app = Fastify({
-    logger: true
+const app = Fastify();
+
+app.register(multipart);
+
+app.register(require('@fastify/static'), {
+    root: resolve(__dirname, '../uploads'),
+    prefix: '/uploads',
 });
 
-(async () => {
-    try {
-        await app.listen({ port: 8888 });
-        console.log('Server running port 8888');
-    } catch (err) {
-        console.log('Error: ', err);
-    }
-})();
+app.register(cors, {
+    origin: true,
+});
+
+app.register(jwt, {
+    secret: 'appraiser',
+});
+
+app
+  .listen({
+    port: 3333,
+    host: '0.0.0.0',
+  })
+  .then(() => {
+    console.log('🚀 HTTP server running on port http://localhost:3333')
+  })
